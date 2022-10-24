c922f37491eee4e799750d5ff2b48c81780acec4
panjacek <panjacek@users.noreply.github.com>
2022-10-24 16:07:59 +0000

    deploy: 8b1de59c748d93faf0d397cd1da06ca09986b92f

--- rustdoc/ltn/design_ltn/sidebar-items.js
+++ rustdoc/ltn/design_ltn/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["advanced_panel",""],["help",""],["setup_editing",""]],"struct":[["DesignLTN",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["edit_mode",""],["help",""],["launch_advanced",""],["make_bottom_panel",""],["setup_editing",""]],"struct":[["DesignLTN",""]]};
